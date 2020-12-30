14594d2783b390cdea143c21d50411c316d2c9eb
vinodh1988 <k.c.vinodh@hotmail.com>
2020-12-30 12:24:49 +0530

    menubar-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { SubscriptionComponent } from './pages/subscription/subscription.component';
+import { CommentboxComponent } from './components/commentbox/commentbox.component';
+import { CommentComponent } from './components/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent,
+    HomeComponent,
+    ContactComponent,
+    SubscriptionComponent,
+    CommentboxComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/commentbox/commentbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commentbox',
+  templateUrl: './commentbox.component.html',
+  styleUrls: ['./commentbox.component.css']
+})
+export class CommentboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
