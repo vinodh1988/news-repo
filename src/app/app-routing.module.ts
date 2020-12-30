9fda9c44220a631d0c134591a91d5161903bb8ce
vinodh1988 <k.c.vinodh@hotmail.com>
2020-12-30 12:48:52 +0530

    routing-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SubscriptionComponent } from './pages/subscription/subscription.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component: HomeComponent},
+  {path:"reachus",component: ContactComponent},
+  {path:"subscription",component:SubscriptionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
